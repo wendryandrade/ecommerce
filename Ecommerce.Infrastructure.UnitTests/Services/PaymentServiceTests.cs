a50eba87ccdd632b7e7a8cb64d2112465df9d73b
Wendrya Andrade <wendrya1@gmail.com>
2025-08-27 08:08:27 -0300

    feat: Atualiza README

--- Ecommerce.Infrastructure.UnitTests/Services/PaymentServiceTests.cs
+++
@@ -1,7 +0,0 @@
-using Xunit;
-
-namespace Ecommerce.Infrastructure.UnitTests.Services
-{
-    // Placeholder: N�o h� PaymentService gen�rico na solu��o, usamos StripePaymentService com testes pr�prios.
-    public class PaymentServiceTests { }
-}
